c34163333e63f96ed37420710681578fd15b4900
嘉伟 张 <1442730983@qq.com>
2021-03-04 12:11:46 +0800

    窗体计算器

---
+++ 第一次作业/calculator/calculator/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.multiButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.divButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("黑体", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(190, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "简易计算器";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("楷体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(140, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "运算数1：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("楷体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(140, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "运算数2：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("楷体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(86, 337);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 30);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "运算：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(315, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 46);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(315, 225);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 46);
+            this.textBox2.TabIndex = 5;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.addButton.Location = new System.Drawing.Point(195, 321);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(55, 56);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "+";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // multiButton
+            // 
+            this.multiButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.multiButton.Location = new System.Drawing.Point(384, 321);
+            this.multiButton.Name = "multiButton";
+            this.multiButton.Size = new System.Drawing.Size(55, 56);
+            this.multiButton.TabIndex = 7;
+            this.multiButton.Text = "×";
+            this.multiButton.UseVisualStyleBackColor = true;
+            this.multiButton.Click += new System.EventHandler(this.multiButton_Click);
+            // 
+            // minusButton
+            // 
+            this.minusButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.minusButton.Location = new System.Drawing.Point(288, 321);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(55, 56);
+            this.minusButton.TabIndex = 8;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = true;
+            this.minusButton.Click += new System.EventHandler(this.minusButton_Click);
+            // 
+            // divButton
+            // 
+            this.divButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.divButton.Location = new System.Drawing.Point(475, 321);
+            this.divButton.Name = "divButton";
+            this.divButton.Size = new System.Drawing.Size(55, 56);
+            this.divButton.TabIndex = 9;
+            this.divButton.Text = "÷";
+            this.divButton.UseVisualStyleBackColor = true;
+            this.divButton.Click += new System.EventHandler(this.divButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("楷体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(125, 425);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 30);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "运算结果：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(315, 417);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(154, 46);
+            this.textBox3.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 573);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.divButton);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.multiButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button multiButton;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button divButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ 第一次作业/calculator/calculator/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            double value1 = 0;
+            double value2 = 0;
+            double  result = 0;
+            double.TryParse(textBox1.Text, out value1);
+            double.TryParse(textBox2.Text, out value2);
+
+            result = value1 + value2;
+
+            textBox3.Text = result.ToString();
+        }
+
+        private void minusButton_Click(object sender, EventArgs e)
+        {
+            double value1 = 0;
+            double value2 = 0;
+            double result = 0;
+            double.TryParse(textBox1.Text, out value1);
+            double.TryParse(textBox2.Text, out value2);
+
+            result = value1 - value2;
+
+            textBox3.Text = result.ToString();
+        }
+
+        private void multiButton_Click(object sender, EventArgs e)
+        {
+            double value1 = 0;
+            double value2 = 0;
+            double result = 0;
+            double.TryParse(textBox1.Text, out value1);
+            double.TryParse(textBox2.Text, out value2);
+
+            result = value1 * value2;
+
+            textBox3.Text = result.ToString();
+        }
+
+        private void divButton_Click(object sender, EventArgs e)
+        {
+            double value1 = 0;
+            double value2 = 0;
+            double result = 0;
+            double.TryParse(textBox1.Text, out value1);
+            double.TryParse(textBox2.Text, out value2);
+
+            result = value1 / value2;
+
+            textBox3.Text = result.ToString();
+        }
+    }
+}
