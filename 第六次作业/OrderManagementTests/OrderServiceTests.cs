a434c77686808128646705b1c578d70753c573aa
JiaweiZhang <jiawei_whucs@whu.edu.cn>
2021-04-12 18:51:34 +0800

    第六次作业

---
+++ 第六次作业/OrderManagementTests/OrderServiceTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Order;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            //Assert.Fail();
+            OrderService orderService = new OrderService();
+            orderService.AddOrder("111");
+        }
+
+        [TestMethod()]
+        public void RemoveOrderTest()
+        {
+            //Assert.Fail();
+            OrderService orderService = new OrderService();
+            orderService.AddOrder("111");
+            orderService.RemoveOrder("111");
+
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            //Assert.Fail();
+            OrderService orderService = new OrderService();
+            orderService.AddOrder("111");
+            orderService.AddOrder("222");
+            orderService.ToString();
+        }
+
+        [TestMethod()]
+        public void AmendOrderTest()
+        {
+            //Assert.Fail();
+            OrderService orderService = new OrderService();
+            orderService.AddOrder("111");
+            orderService.AmendOrder("111", "222");
+        }
+
+        [TestMethod()]
+        public void SortByNumberTest()
+        {
+            //Assert.Fail();
+            OrderService orderService = new OrderService();
+            orderService.AddOrder("111");
+            orderService.AddOrder("222");
+            orderService.SortByNumber();
+        }
+
+        [TestMethod()]
+        public void SearchByNumberTest()
+        {
+            //Assert.Fail();
+            OrderService orderService = new OrderService();
+            orderService.AddOrder("111");
+            orderService.AddOrder("222");
+            orderService.SearchByNumber("111");
+
+        }
+
+        [TestMethod()]
+        public void SearchByNameTest()
+        {
+            //Assert.Fail();
+            OrderService orderService = new OrderService();
+            orderService.AddOrder("111");
+            orderService.AddOrder("222");
+            orderService.orders[0].AddDetail("鸡蛋", 10, 1);
+            orderService.orders[0].AddDetail("牛奶", 5, 3);
+            orderService.orders[1].AddDetail("鸡蛋", 20, 1);
+            orderService.orders[1].AddDetail("面包", 5, 2);
+
+        }
+
+        [TestMethod()]
+        public void SearchByPriceTest()
+        {
+            //Assert.Fail();
+            OrderService orderService = new OrderService();
+            orderService.AddOrder("111");
+            orderService.AddOrder("222");
+            orderService.orders[0].AddDetail("鸡蛋", 10, 1);
+            orderService.orders[0].AddDetail("牛奶", 5, 3);
+            orderService.orders[1].AddDetail("鸡蛋", 20, 1);
+            orderService.orders[1].AddDetail("面包", 5, 2);
+            orderService.SearchByPrice(20);
+        }
+
+        [TestMethod()]
+        public void ExportTest()
+        {
+            //Assert.Fail();
+            OrderService orderService = new OrderService();
+            orderService.AddOrder("111");
+            orderService.orders[0].AddDetail("鸡蛋", 10, 1);
+            orderService.orders[0].AddDetail("牛奶", 5, 3);
+            orderService.Export();
+        }
+
+        [TestMethod()]
+        public void InportTest()
+        {
+            //ssert.Fail();
+            OrderService orderService = new OrderService();
+            orderService.AddOrder("111");
+            orderService.orders[0].AddDetail("鸡蛋", 10, 1);
+            orderService.orders[0].AddDetail("牛奶", 5, 3);
+            orderService.Export();
+            orderService.Inport();
+        }
+    }
+}
---
+++ 第六次作业/第五次作业/OrderService.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Order
+{
+	public class OrderService
+	{
+		public List<Order> orders;
+		public OrderService()
+		{
+			orders = new List<Order>();
+		}
+		public void AddOrder(string number)
+		{   //添加订单
+			Order newOrder = new Order(number);
+			try
+			{
+				foreach (Order ord in orders)
+				{
+					if (ord.Equals(newOrder)) throw new ApplicationException();
+					//orders.Add(newOrder);
+				}
+			}
+			catch { Console.WriteLine($"{number} has already existed"); }
+			orders.Add(newOrder);
+		}
+
+		public void RemoveOrder(string number)//删除订单
+		{
+			foreach (Order ord in orders)
+				if (ord.number == number) orders.Remove(ord);
+			//catch { Console.WriteLine($"Can not find {number}"); }
+		}
+		public override string ToString()                 //查看所有订单
+		{
+			string result = "";
+			foreach (Order ord in orders)
+				result = result + ord.number + "\t" + ord.OrderTime() + "\n";
+			return result;
+		}
+
+		public void AmendOrder(string oldNum, string newNum) //修改订单
+		{
+			try
+			{
+				foreach (Order ord in orders)
+					if (ord.number == oldNum) ord.number = newNum;
+			}
+			catch { Console.WriteLine($"Can not find {oldNum}"); }
+		}
+
+		public void SortByNumber()  //按照订单号排序
+		{
+			orders.Sort();
+		}
+
+		public List<Order> SearchByNumber(string number) //按照订单号查找
+		{
+			var result = from s in orders
+						 where s.number == number
+						 orderby s.totalPrice
+						 select s;
+			return result.ToList();
+		}
+		public List<string> SearchByName(string name)  //按照商品名称查找
+        {
+			List<string> resultOrders = new List<string>();
+
+			foreach (Order ord in orders)
+			{
+				var aResult = from s in ord.orderDetails
+							 where s.commodityName == name
+							 select s.InWhichOrder;
+				resultOrders.AddRange(aResult.ToList());
+			}
+			return resultOrders;
+        }
+
+		public List<Order> SearchByPrice(int price)  //按照订单金额查找
+		{
+			var result = from s in orders
+						 where s.totalPrice <=price
+						 select s;
+			return result.ToList();
+		}
+
+		public void Export()
+		{ XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+			using (FileStream fs = new FileStream("orders.xml", FileMode.Create))
+            {
+				xmlSerializer.Serialize(fs, orders);
+				Console.WriteLine("Orders have serialized as XML:");
+			}
+			Console.WriteLine(File.ReadAllText("orders.xml"));
+			
+		}
+
+		public void Inport()
+		{ XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+			using (FileStream fs = new FileStream("orders.xml", FileMode.Open))
+            {
+				List<Order> orders2 = (List<Order>)xmlSerializer.Deserialize(fs);
+				Console.WriteLine("Orders have deserialized from xml:");
+				foreach(Order ord in orders2)
+				{ Console.WriteLine(ord); }
+			}
+			
+		}
+	}
+}
